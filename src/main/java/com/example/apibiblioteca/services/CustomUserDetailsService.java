74e74b87cffbebde015cd14e579f900a5efd9eaf
Cora Lara <coralara@MacBook-Air-de-Cora.local>
2026-01-02 12:08:51 +0100

    Versión final con JWT lista para pruebas

---
+++ src/main/java/com/example/apibiblioteca/services/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.apibiblioteca.services;
+import com.example.apibiblioteca.repositories.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor //crea un constructor con todos los parametros requeridos (inyecta el autowired)
+public class CustomUserDetailsService implements UserDetailsService {
+
+    //@Autowired (solo si quito el RequiredArgsConstructor )
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        // Spring Security llama a este método al hacer login
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+    }
+}
